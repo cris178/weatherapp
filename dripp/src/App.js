817ee3521ff8cabdbc4444e84259d7079dda5bbe
Cristian <cris178@outlook.com>
2019-11-23 22:09:06 -0800

    Fetching

--- dripp/src/App.js
+++ dripp/src/App.js
@@ -8,9 +8,31 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      city: ""
+      city: "",
+      user: null
     };
     this.setCity = this.setCity.bind(this);
+  }
+
+  componentDidUpdate(prevState) {
+    if (this.state.city !== prevState.city) {
+      if (this.state.city !== "") {
+        if (this.state.user === null) {
+          const APPID = "7b39d84acc855216bc1552e6a482bd4e";
+          let API =
+            "http://api.openweathermap.org/data/2.5/forecast?q=" +
+            this.state.city +
+            "&units=imperial&id=524901&APPID=" +
+            APPID;
+          console.log(API);
+
+          fetch(API)
+            .then(response => response.json())
+            .then(responseJson => this.setState({ user: responseJson }));
+          console.log("Set API right");
+        }
+      }
+    }
   }
 
   setCity(arg) {
@@ -19,14 +41,19 @@ class App extends React.Component {
       city: arg
     });
   }
+
   render() {
     console.log("State after Passed up: " + this.state.city);
+    let local = this.state.city;
+    console.log("API: " + local[1]);
+    let cityName = this.state.city;
     return (
       <div className="App">
         <header>
           <h1>Dripp</h1>
         </header>
         <SearchBar passUp={this.setCity} />
+        <h1>{cityName}</h1>
         <Cards />
       </div>
     );
